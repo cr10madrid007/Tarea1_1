e458f5680c323310aafda10306a5ca53ef11e195
cr10madrid007 <cristofermadrid2010@hotmail.com>
2022-01-27 23:15:28 -0600

    first commit

---
+++ Tarea1_1/Tarea1_1/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        private async void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.Procesos
+            {
+                Resultado = Convert.ToInt32(this.txtN1.Text) + Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+        }
+
+        private async void btnRestar_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.Procesos
+            {
+                Resultado = Convert.ToInt32(this.txtN1.Text) - Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+
+        private async void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.Procesos
+            {
+                Resultado = Convert.ToInt32(this.txtN1.Text) / Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+        }
+
+        private async void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.Procesos
+            {
+                Resultado = Convert.ToInt32(this.txtN1.Text) * Convert.ToInt32(this.txtN2.Text)
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+        }
+    }
+}
---
+++ Tarea1_1/Tarea1_1/Models/Procesos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1_1.Models
+{
+    public class Procesos
+    {
+        public Int32 Resultado { get; set; }
+    }
+}
